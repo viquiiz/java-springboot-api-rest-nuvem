38e044fd53eb7793f5eeadcc0019e2b30fcce96f
Victória Zanella <vzanella@outlook.com.br>
2023-10-22 01:44:42 -0300

    initial commit

---
+++ src/main/java/me/dio/java/Aplication.java
@@ -0,0 +1,13 @@
+package me.dio.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/java/JavaSpringbootApiRestNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootApiRestNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
